ada153d4d8ed184fade8f9fd132424313743d46f
John Higgins <johnmatthiggins@gmail.com>
2022-10-08 14:41:25 -0700

    Created Factorial and Fibonacci functions

---
+++ fac.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+printf "1\n2\n" | cat - >> out\
+        && seq 3 $1 |\
+        xargs -I "{}" sh -c 'echo $(expr "{}" \* $(tail -n 1 out)) >> out'\
+        && cat out && rm out
