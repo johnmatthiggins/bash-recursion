e9dfc994d51acc46f387048ae22e765971ef2191
John Higgins <johnmatthiggins@gmail.com>
2022-10-18 13:33:30 -0700

    Made files actually one line

--- fac.sh
+++ fac.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-printf "1\n2\n" | cat - >> out\
-        && seq 3 $1 |\
-        xargs -I "{}" sh -c 'echo $(expr "{}" \* $(tail -n 1 out)) >> out'\
-        && cat out && rm out
+printf "1\n2\n" | cat - >> out && seq 3 $1 | xargs -I "{}" sh -c 'echo $(expr "{}" \* $(tail -n 1 out)) >> out' && cat out && rm out
--- fib.sh
+++ fib.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-printf "0\n1\n" | \
-    cat - >> fib_out &&\
-        echo "$(expr $(tail -n 1 fib_out) + $(tail -n 2 fib_out | head -n 1))" >> fib_out\
-        && seq $(expr $1 - 2) |\
-        xargs -n 1 sh -c 'echo $(expr $(tail -n 1 fib_out) + $(tail -n 2 fib_out | head -n 1)) >> fib_out'\
-        && cat fib_out && rm fib_out
+printf "0\n1\n" | cat - >> fib_out && echo "$(expr $(tail -n 1 fib_out) + $(tail -n 2 fib_out | head -n 1))" >> fib_out && seq $(expr $1 - 2) | xargs -n 1 sh -c 'echo $(expr $(tail -n 1 fib_out) + $(tail -n 2 fib_out | head -n 1)) >> fib_out' && cat fib_out && rm fib_out
