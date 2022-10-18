6d53609e5f8ae91a5928cb44c27c0a40676fe5fa
John Higgins <johnmatthiggins@gmail.com>
2022-10-18 13:32:28 -0700

    Added TODO

--- pi.sh
+++ pi.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+# TODO: actually finish this.
 # export FN="$1" && echo $(tail -n 1 $FN) >> sum && seq 2 $(cat $FN | wc -l) | xargs -I '{}' sh -c 'echo $(expr $(tail -n 1 sum) + $(tail -n "{}" $FN | head -n 1)) >> sum' && tail -n 1 sum && rm sum
 export FN="num" && seq 1000 >> $FN && perl -e "print ($(echo $(tail -n 1 $FN) >> sum && seq 2 $(cat $FN | wc -l) | xargs -I '{}' sh -c 'echo $(expr $(tail -n 1 sum) + $(tail -n "{}" $FN | head -n 1)) >> sum' && tail -n 1 sum && rm sum) / $(cat $FN | wc -l));" && rm $FN
