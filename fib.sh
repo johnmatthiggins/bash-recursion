bfdb464691d3b17722b13830025295e5428abbd7
John Higgins <johnmatthiggins@gmail.com>
2024-02-15 23:25:58 -0800

    fixed script so we don't need to cat to get output

--- fib.sh
+++ fib.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-
-printf "0\n1\n" | cat - >> fib_out && seq $(expr $1 - 2) | xargs -n 1 sh -c 'echo $(expr $(tail -n 1 fib_out) + $(tail -n 2 fib_out | head -n 1)) >> fib_out' && cat fib_out && rm fib_out
+printf "1\n1\n" | tee -a fib_out && seq $(expr $1 - 2) | xargs -n 1 sh -c 'echo $(expr $(tail -n 1 fib_out) + $(tail -n 2 fib_out | head -n 1)) | tee -a fib_out' && rm fib_out
